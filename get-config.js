1fc3b84283ae182a510cc353123cd4c01074352b
IronLuffy55 <IronLuffy55@users.noreply.github.com>
2018-08-13 18:22:19 -0400

    Added shim for creating multiple AppSync resources

--- get-config.js
+++ get-config.js
@@ -1,60 +1,72 @@
-const fs = require('fs');
-const path = require('path');
-const {
-  mapObjIndexed, pipe, values, merge,
-} = require('ramda');
+const fs = require("fs");
+const path = require("path");
+const { mapObjIndexed, pipe, values, merge } = require("ramda");
 
 const objectToArrayWithNameProp = pipe(
   mapObjIndexed((item, key) => merge({ name: key }, item)),
-  values,
+  values
 );
 
 module.exports = (config, provider, servicePath) => {
   if (
     !(
-      config.authenticationType === 'API_KEY' ||
-      config.authenticationType === 'AWS_IAM' ||
-      config.authenticationType === 'AMAZON_COGNITO_USER_POOLS' ||
-      config.authenticationType === 'OPENID_CONNECT'
+      config.authenticationType === "API_KEY" ||
+      config.authenticationType === "AWS_IAM" ||
+      config.authenticationType === "AMAZON_COGNITO_USER_POOLS" ||
+      config.authenticationType === "OPENID_CONNECT"
     )
   ) {
-    throw new Error('appSync property `authenticationType` is missing or invalid.');
+    throw new Error(
+      "appSync property `authenticationType` is missing or invalid."
+    );
   }
   if (!config.serviceRole) {
-    throw new Error('appSync property `serviceRole` is required.');
+    throw new Error("appSync property `serviceRole` is required.");
   }
-  if (config.authenticationType === 'AMAZON_COGNITO_USER_POOLS' && !config.userPoolConfig) {
-    throw new Error('appSync property `userPoolConfig` is required when authenticationType `AMAZON_COGNITO_USER_POOLS` is chosen.');
+  if (
+    config.authenticationType === "AMAZON_COGNITO_USER_POOLS" &&
+    !config.userPoolConfig
+  ) {
+    throw new Error(
+      "appSync property `userPoolConfig` is required when authenticationType `AMAZON_COGNITO_USER_POOLS` is chosen."
+    );
   }
-  if (config.authenticationType === 'OPENID_CONNECT' && !config.openIdConnectConfig) {
-    throw new Error('appSync property `openIdConnectConfig` is required when authenticationType `OPENID_CONNECT` is chosenXXX.');
+  if (
+    config.authenticationType === "OPENID_CONNECT" &&
+    !config.openIdConnectConfig
+  ) {
+    throw new Error(
+      "appSync property `openIdConnectConfig` is required when authenticationType `OPENID_CONNECT` is chosenXXX."
+    );
   }
   if (config.logConfig && !config.logConfig.loggingRoleArn) {
-    throw new Error('logConfig property `loggingRoleArn` is required when logConfig exists.');
+    throw new Error(
+      "logConfig property `loggingRoleArn` is required when logConfig exists."
+    );
   }
   if (config.logConfig && !config.logConfig.level) {
-    throw new Error('logConfig property `level` must be NONE, ERROR, or ALL when logConfig exists.');
+    throw new Error(
+      "logConfig property `level` must be NONE, ERROR, or ALL when logConfig exists."
+    );
   }
-  if (config.substitutions && typeof config.substitutions !== 'object') {
-    throw new Error('substitutions property must be an object');
+  if (config.substitutions && typeof config.substitutions !== "object") {
+    throw new Error("substitutions property must be an object");
   }
 
-  const mappingTemplatesLocation = config.mappingTemplatesLocation || 'mapping-templates';
+  const mappingTemplatesLocation =
+    config.mappingTemplatesLocation || "mapping-templates";
   const mappingTemplates = config.mappingTemplates || [];
 
-  const schemaPath = path.join(
-    servicePath,
-    config.schema || 'schema.graphql',
-  );
+  const schemaPath = path.join(servicePath, config.schema || "schema.graphql");
 
   const schemaContent = fs.readFileSync(schemaPath, {
-    encoding: 'utf8',
+    encoding: "utf8"
   });
 
   const dataSources = objectToArrayWithNameProp(config.dataSources);
 
   return {
-    name: config.name || 'api',
+    name: config.name || "api",
     apiId: config.apiId,
     apiKey: config.apiKey,
     region: provider.region,
@@ -69,5 +81,6 @@ module.exports = (config, provider, servicePath) => {
     mappingTemplates,
     logConfig: config.logConfig,
     substitutions: config.substitutions || {},
+    groups: config.groups
   };
 };
--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const {
-  validateSchema, printError, parse, buildASTSchema,
-} = require('graphql');
-const getConfig = require('./get-config');
+  validateSchema,
+  printError,
+  parse,
+  buildASTSchema
+} = require("graphql");
+const getConfig = require("./get-config");
 
-const MIGRATION_DOCS = 'https://github.com/sid88in/serverless-appsync-plugin/blob/master/README.md#cfn-migration';
+const MIGRATION_DOCS =
+  "https://github.com/sid88in/serverless-appsync-plugin/blob/master/README.md#cfn-migration";
 
 class ServerlessAppsyncPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-    this.provider = this.serverless.getProvider('aws');
+    this.provider = this.serverless.getProvider("aws");
     this.commands = {
-      'delete-appsync': {
-        usage: 'Helps you delete AppSync API',
-        lifecycleEvents: ['delete'],
+      "delete-appsync": {
+        usage: "Helps you delete AppSync API",
+        lifecycleEvents: ["delete"]
       },
-      'deploy-appsync': {
-        usage: 'DEPRECATED: Helps you deploy AppSync API',
-        lifecycleEvents: ['deploy'],
+      "deploy-appsync": {
+        usage: "DEPRECATED: Helps you deploy AppSync API",
+        lifecycleEvents: ["deploy"]
       },
-      'update-appsync': {
-        usage: 'DEPRECATED: Helps you update AppSync API',
-        lifecycleEvents: ['update'],
-      },
+      "update-appsync": {
+        usage: "DEPRECATED: Helps you update AppSync API",
+        lifecycleEvents: ["update"]
+      }
     };
 
     const generateMigrationErrorMessage = command => () => {
-      throw new this.serverless.classes.Error(`serverless-appsync: ${command} `
-        + `is no longer supported. See ${MIGRATION_DOCS} for more information`);
+      throw new this.serverless.classes.Error(
+        `serverless-appsync: ${command} ` +
+          `is no longer supported. See ${MIGRATION_DOCS} for more information`
+      );
     };
     this.hooks = {
-      'before:deploy:initialize': () => this.validateSchema(),
-      'delete-appsync:delete': () => this.deleteGraphQLEndpoint(),
-      'deploy-appsync:deploy': generateMigrationErrorMessage('deploy-appsync'),
-      'update-appsync:update': generateMigrationErrorMessage('update-appsync'),
-      'before:deploy:deploy': () => this.addResources(),
+      "before:deploy:initialize": () => this.validateSchema(),
+      "delete-appsync:delete": () => this.deleteGraphQLEndpoint(),
+      "deploy-appsync:deploy": generateMigrationErrorMessage("deploy-appsync"),
+      "update-appsync:update": generateMigrationErrorMessage("update-appsync"),
+      "before:deploy:deploy": () => this.addResources()
     };
   }
 
@@ -44,7 +50,7 @@ class ServerlessAppsyncPlugin {
     return getConfig(
       this.serverless.service.custom.appSync,
       this.serverless.service.provider,
-      this.serverless.config.servicePath,
+      this.serverless.config.servicePath
     );
   }
 
@@ -74,29 +80,35 @@ class ServerlessAppsyncPlugin {
       return;
     }
 
-    errors.forEach((error) => {
+    errors.forEach(error => {
       this.serverless.cli.log(printError(error));
     });
-    throw new this.serverless.classes.Error('Cannot proceed invalid graphql SDL');
+    throw new this.serverless.classes.Error(
+      "Cannot proceed invalid graphql SDL"
+    );
   }
 
   deleteGraphQLEndpoint() {
     const config = this.loadConfig();
     const { apiId } = config;
     if (!apiId) {
-      throw new this.serverless.classes.Error('serverless-appsync: no apiId is defined. If you are not '
-        + `migrating from a previous version of the plugin this is expected.  See ${MIGRATION_DOCS} '
-        + 'for more information`);
+      throw new this.serverless.classes.Error(
+        "serverless-appsync: no apiId is defined. If you are not " +
+          `migrating from a previous version of the plugin this is expected.  See ${MIGRATION_DOCS} '
+        + 'for more information`
+      );
     }
 
-    this.serverless.cli.log('Deleting GraphQL Endpoint...');
+    this.serverless.cli.log("Deleting GraphQL Endpoint...");
     return this.provider
-      .request('AppSync', 'deleteGraphqlApi', {
-        apiId,
+      .request("AppSync", "deleteGraphqlApi", {
+        apiId
       })
-      .then((data) => {
+      .then(data => {
         if (data) {
-          this.serverless.cli.log(`Successfully deleted GraphQL Endpoint: ${apiId}`);
+          this.serverless.cli.log(
+            `Successfully deleted GraphQL Endpoint: ${apiId}`
+          );
         }
       });
   }
@@ -105,138 +117,209 @@ class ServerlessAppsyncPlugin {
     const config = this.loadConfig();
 
     if (config.apiId) {
-      this.serverless.cli.log('WARNING: serverless-appsync has been updated in a breaking way and your '
-        + 'service is configured using a reference to an existing apiKey in '
-        + '`custom.appSync` which is used in the legacy deploy scripts. This deploy will create '
-        + `new graphql resources and WILL NOT update your existing api. See ${MIGRATION_DOCS} for `
-        + 'more information');
+      this.serverless.cli.log(
+        "WARNING: serverless-appsync has been updated in a breaking way and your " +
+          "service is configured using a reference to an existing apiKey in " +
+          "`custom.appSync` which is used in the legacy deploy scripts. This deploy will create " +
+          `new graphql resources and WILL NOT update your existing api. See ${MIGRATION_DOCS} for ` +
+          "more information"
+      );
     }
 
-    const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+    const resources = this.serverless.service.provider
+      .compiledCloudFormationTemplate.Resources;
     Object.assign(resources, this.getGraphQlApiEndpointResource(config));
     Object.assign(resources, this.getApiKeyResources(config));
     Object.assign(resources, this.getGraphQLSchemaResource(config));
     Object.assign(resources, this.getDataSourceResources(config));
     Object.assign(resources, this.getResolverResources(config));
 
-    const outputs = this.serverless.service.provider.compiledCloudFormationTemplate.Outputs;
+    const outputs = this.serverless.service.provider
+      .compiledCloudFormationTemplate.Outputs;
     Object.assign(outputs, this.getGraphQlApiOutputs(config));
     Object.assign(outputs, this.getApiKeyOutputs(config));
   }
 
   getGraphQlApiEndpointResource(config) {
+    if (config.groups && Array.isArray(config.groups)) {
+      const prefix = "GraphQlApi";
+      const resource = {};
+      config.groups.forEach((apiConfig, i) => {
+        const logicalId = prefix + (i || "");
+        resource[logicalId] = {
+          Type: "AWS::AppSync::GraphQLApi",
+          Properties: {
+            Name: apiConfig.name,
+            AuthenticationType: apiConfig.authenticationType,
+            UserPoolConfig:
+              apiConfig.authenticationType !== "AMAZON_COGNITO_USER_POOLS"
+                ? undefined
+                : {
+                    AwsRegion: apiConfig.region,
+                    DefaultAction: apiConfig.userPoolConfig.defaultAction,
+                    UserPoolId: apiConfig.userPoolConfig.userPoolId
+                  },
+            OpenIDConnectConfig:
+              apiConfig.authenticationType !== "OPENID_CONNECT"
+                ? undefined
+                : {
+                    Issuer: apiConfig.openIdConnectConfig.issuer,
+                    ClientId: apiConfig.openIdConnectConfig.clientId,
+                    IatTTL: apiConfig.openIdConnectConfig.iatTTL,
+                    AuthTTL: apiConfig.openIdConnectConfig.authTTL
+                  },
+            LogConfig: !apiConfig.logConfig
+              ? undefined
+              : {
+                  CloudWatchLogsRoleArn: apiConfig.logConfig.loggingRoleArn,
+                  FieldLogLevel: apiConfig.logConfig.level
+                }
+          }
+        };
+      });
+      return resource;
+    }
     return {
       GraphQlApi: {
-        Type: 'AWS::AppSync::GraphQLApi',
+        Type: "AWS::AppSync::GraphQLApi",
         Properties: {
           Name: config.name,
           AuthenticationType: config.authenticationType,
-          UserPoolConfig: config.authenticationType !== 'AMAZON_COGNITO_USER_POOLS' ? undefined : {
-            AwsRegion: config.region,
-            DefaultAction: config.userPoolConfig.defaultAction,
-            UserPoolId: config.userPoolConfig.userPoolId,
-          },
-          OpenIDConnectConfig: config.authenticationType !== 'OPENID_CONNECT' ? undefined : {
-            Issuer: config.openIdConnectConfig.issuer,
-            ClientId: config.openIdConnectConfig.clientId,
-            IatTTL: config.openIdConnectConfig.iatTTL,
-            AuthTTL: config.openIdConnectConfig.authTTL,
-          },
-          LogConfig: !config.logConfig ? undefined : {
-            CloudWatchLogsRoleArn: config.logConfig.loggingRoleArn,
-            FieldLogLevel: config.logConfig.level,
-          },
-        },
-      },
+          UserPoolConfig:
+            config.authenticationType !== "AMAZON_COGNITO_USER_POOLS"
+              ? undefined
+              : {
+                  AwsRegion: config.region,
+                  DefaultAction: config.userPoolConfig.defaultAction,
+                  UserPoolId: config.userPoolConfig.userPoolId
+                },
+          OpenIDConnectConfig:
+            config.authenticationType !== "OPENID_CONNECT"
+              ? undefined
+              : {
+                  Issuer: config.openIdConnectConfig.issuer,
+                  ClientId: config.openIdConnectConfig.clientId,
+                  IatTTL: config.openIdConnectConfig.iatTTL,
+                  AuthTTL: config.openIdConnectConfig.authTTL
+                },
+          LogConfig: !config.logConfig
+            ? undefined
+            : {
+                CloudWatchLogsRoleArn: config.logConfig.loggingRoleArn,
+                FieldLogLevel: config.logConfig.level
+              }
+        }
+      }
     };
   }
 
   getApiKeyResources(config) {
-    if (config.authenticationType !== 'API_KEY') {
+    if (config.authenticationType !== "API_KEY") {
       return {};
     }
     return {
       GraphQlApiKeyDefault: {
-        Type: 'AWS::AppSync::ApiKey',
+        Type: "AWS::AppSync::ApiKey",
         Properties: {
-          ApiId: { 'Fn::GetAtt': ['GraphQlApi', 'ApiId'] },
-          Description: 'serverless-appsync-plugin: Default',
-          Expires: Math.floor(Date.now() / 1000) + (365 * 24 * 60 * 60),
-        },
-      },
+          ApiId: { "Fn::GetAtt": ["GraphQlApi", "ApiId"] },
+          Description: "serverless-appsync-plugin: Default",
+          Expires: Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60
+        }
+      }
     };
   }
 
   getDataSourceResources(config) {
     return config.dataSources.reduce((acc, ds) => {
       const resource = {
-        Type: 'AWS::AppSync::DataSource',
+        Type: "AWS::AppSync::DataSource",
         Properties: {
-          ApiId: { 'Fn::GetAtt': ['GraphQlApi', 'ApiId'] },
+          ApiId: { "Fn::GetAtt": ["GraphQlApi", "ApiId"] },
           Name: ds.name,
           Description: ds.description,
           Type: ds.type,
-          ServiceRoleArn: ds.type === 'NONE' ? undefined : ds.config.serviceRoleArn,
-        },
+          ServiceRoleArn:
+            ds.type === "NONE" ? undefined : ds.config.serviceRoleArn
+        }
       };
-      if (ds.type === 'AWS_LAMBDA') {
+      if (ds.type === "AWS_LAMBDA") {
         resource.Properties.LambdaConfig = {
-          LambdaFunctionArn: ds.config.lambdaFunctionArn,
+          LambdaFunctionArn: ds.config.lambdaFunctionArn
         };
-      } else if (ds.type === 'AMAZON_DYNAMODB') {
+      } else if (ds.type === "AMAZON_DYNAMODB") {
         resource.Properties.DynamoDBConfig = {
           AwsRegion: ds.config.region || config.region,
           TableName: ds.config.tableName,
-          UseCallerCredentials: !!ds.config.useCallerCredentials,
+          UseCallerCredentials: !!ds.config.useCallerCredentials
         };
-      } else if (ds.type === 'AMAZON_ELASTICSEARCH') {
+      } else if (ds.type === "AMAZON_ELASTICSEARCH") {
         resource.Properties.ElasticsearchConfig = {
           AwsRegion: ds.config.region || config.region,
-          Endpoint: ds.config.endpoint,
+          Endpoint: ds.config.endpoint
         };
-      } else if (ds.type === 'HTTP') {
+      } else if (ds.type === "HTTP") {
         resource.Properties.HttpConfig = {
-          Endpoint: ds.config.endpoint,
+          Endpoint: ds.config.endpoint
         };
-      } else if (ds.type !== 'NONE') {
-        throw new this.serverless.classes.Error(`Data Source Type not supported: '${ds.type}`);
+      } else if (ds.type !== "NONE") {
+        throw new this.serverless.classes.Error(
+          `Data Source Type not supported: '${ds.type}`
+        );
       }
-      return Object.assign({}, acc, { [this.getDataSourceCfnName(ds.name)]: resource });
+      return Object.assign({}, acc, {
+        [this.getDataSourceCfnName(ds.name)]: resource
+      });
     }, {});
   }
 
   getGraphQLSchemaResource(config) {
     return {
       GraphQlSchema: {
-        Type: 'AWS::AppSync::GraphQLSchema',
+        Type: "AWS::AppSync::GraphQLSchema",
         Properties: {
           Definition: config.schema,
-          ApiId: { 'Fn::GetAtt': ['GraphQlApi', 'ApiId'] },
-        },
-      },
+          ApiId: { "Fn::GetAtt": ["GraphQlApi", "ApiId"] }
+        }
+      }
     };
   }
 
   getResolverResources(config) {
     return config.mappingTemplates.reduce((acc, tpl) => {
-      const reqTemplPath = path.join(config.mappingTemplatesLocation, tpl.request);
-      const respTemplPath = path.join(config.mappingTemplatesLocation, tpl.response);
-      const requestTemplate = fs.readFileSync(reqTemplPath, 'utf8');
-      const responseTemplate = fs.readFileSync(respTemplPath, 'utf8');
+      const reqTemplPath = path.join(
+        config.mappingTemplatesLocation,
+        tpl.request
+      );
+      const respTemplPath = path.join(
+        config.mappingTemplatesLocation,
+        tpl.response
+      );
+      const requestTemplate = fs.readFileSync(reqTemplPath, "utf8");
+      const responseTemplate = fs.readFileSync(respTemplPath, "utf8");
 
       return Object.assign({}, acc, {
-        [`GraphQlResolver${this.getCfnName(tpl.type)}${this.getCfnName(tpl.field)}`]: {
-          Type: 'AWS::AppSync::Resolver',
-          DependsOn: 'GraphQlSchema',
+        [`GraphQlResolver${this.getCfnName(tpl.type)}${this.getCfnName(
+          tpl.field
+        )}`]: {
+          Type: "AWS::AppSync::Resolver",
+          DependsOn: "GraphQlSchema",
           Properties: {
-            ApiId: { 'Fn::GetAtt': ['GraphQlApi', 'ApiId'] },
+            ApiId: { "Fn::GetAtt": ["GraphQlApi", "ApiId"] },
             TypeName: tpl.type,
             FieldName: tpl.field,
-            DataSourceName: { 'Fn::GetAtt': [this.getDataSourceCfnName(tpl.dataSource), 'Name'] },
-            RequestMappingTemplate: this.processTemplate(requestTemplate, config),
-            ResponseMappingTemplate: this.processTemplate(responseTemplate, config),
-          },
-        },
+            DataSourceName: {
+              "Fn::GetAtt": [this.getDataSourceCfnName(tpl.dataSource), "Name"]
+            },
+            RequestMappingTemplate: this.processTemplate(
+              requestTemplate,
+              config
+            ),
+            ResponseMappingTemplate: this.processTemplate(
+              responseTemplate,
+              config
+            )
+          }
+        }
       });
     }, {});
   }
@@ -244,24 +327,24 @@ class ServerlessAppsyncPlugin {
   getGraphQlApiOutputs() {
     return {
       GraphQlApiUrl: {
-        Value: { 'Fn::GetAtt': ['GraphQlApi', 'GraphQLUrl'] },
-      },
+        Value: { "Fn::GetAtt": ["GraphQlApi", "GraphQLUrl"] }
+      }
     };
   }
 
   getApiKeyOutputs(config) {
-    if (config.authenticationType !== 'API_KEY') {
+    if (config.authenticationType !== "API_KEY") {
       return {};
     }
     return {
       GraphQlApiKeyDefault: {
-        Value: { 'Fn::GetAtt': ['GraphQlApiKeyDefault', 'ApiKey'] },
-      },
+        Value: { "Fn::GetAtt": ["GraphQlApiKeyDefault", "ApiKey"] }
+      }
     };
   }
 
   getCfnName(name) {
-    return name.replace(/[^a-zA-Z0-9]/g, '');
+    return name.replace(/[^a-zA-Z0-9]/g, "");
   }
 
   getDataSourceCfnName(name) {
@@ -283,13 +366,14 @@ class ServerlessAppsyncPlugin {
       .filter(value => templateVariables.indexOf(value) > -1)
       .filter((value, index, array) => array.indexOf(value) === index)
       .reduce(
-        (accum, value) => Object.assign(accum, { [value]: config.substitutions[value] }),
-        {},
+        (accum, value) =>
+          Object.assign(accum, { [value]: config.substitutions[value] }),
+        {}
       );
 
     // if there are substitutions for this template then add fn:sub
     if (Object.keys(substitutions).length > 0) {
-      return { 'Fn::Sub': [template, substitutions] };
+      return { "Fn::Sub": [template, substitutions] };
     }
 
     return template;
